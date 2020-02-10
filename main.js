4ea8d5216b4d80e37830272676460a329c1a3a91
Felipe Bianchi <fbs96@live.com>
2020-02-10 11:07:12 -0300

    Arquivos até o término da aula Template Literals

--- main.js
+++ main.js
@@ -1,41 +1,6 @@
-// REST
+const nome = "Felipe";
+const idade = 23;
 
-const usuario = 
-{
-    nome : "Felipe",
-    idade : 23,
-    empresa : "Vigiar"
-}
+console.log("Meu nome é "+nome+" e tenho "+ idade+ " anos" );
 
-const { nome, ...resto } = usuario;
-
-console.log(nome);
-console.log(resto);
-
-const arr = [ 1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-function soma (...params)
-{
-    return params.reduce((total, next)=> total + next);
-}
-
-console.log(soma(1, 3, 4, 5));
-
-// SPREAD
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-console.log(arr3);
-
-const usuario1 = {...usuario, nome: "Sebastião"};
-
-console.log(usuario1);
-
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
