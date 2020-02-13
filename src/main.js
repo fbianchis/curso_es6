b44c61d0e01cc164ff3698d7670efacec938a08d
Felipe Bianchi <fbs96@live.com>
2020-02-13 09:47:46 -0300

    Arquivos até o término da aula Configurando Axios

--- src/main.js
+++ src/main.js
@@ -1,32 +1,20 @@
-const minhaPromise = () => new Promise
-(
-    (resolve, reject) => 
-    {
-        setTimeout(() => 
-        {
-            resolve ('OK')
-        }, 2000);
-    }
-);
+import axios from 'axios'; 
 
-// async function executaPromise ()
-// {
-//     // const response = await minhaPromise();
-//     // console.log(response);
-    
-//     // console.log(await minhaPromise());
-//     // console.log(await minhaPromise());
-//     // console.log(await minhaPromise());
-
-// }
-
-// executaPromise();
-
-const executaPromise = async() =>
+class Api 
 {
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-};
+    static async getUserInfo (username)
+    {
+        try
+        {
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        }
+        catch (err)
+        {
+            console.warn('Erro na API');
+        }
+        
+    }
+}
 
-executaPromise();
+Api.getUserInfo('fbianchis');
