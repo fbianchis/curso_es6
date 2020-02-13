50b49d86b6053c98311bb37a894c65b167aa86c1
Felipe Bianchi <fbs96@live.com>
2020-02-12 21:41:22 -0300

    Arquivos até o término da aula Import/export

--- funcoes.js
+++ funcoes.js
@@ -2,3 +2,13 @@ export function soma (a, b)
 {
     return a + b;
 }
+
+export default function sub (a, b)
+{
+    return a - b;
+}
+
+export function mult (a, b)
+{
+    return a * b;
+}
--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-import { soma }  from './funcoes'
-console.log(soma(1,2));
+//import sub,{ soma as somaF }   from './funcoes'
+//import somaF from './soma'
+import * as funcoes from './funcoes'
+console.log(funcoes);
+console.log(funcoes.soma(1,2));
+// console.log(somaF(1,2));
+// console.log(sub(1,2));
