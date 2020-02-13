bf9a9e52a3eebe46b13dc49b727b73e0080b250a
Felipe Bianchi <fbs96@live.com>
2020-02-13 12:38:55 -0300

    Arquivos até o término da aula Loading e error

--- src/main.js
+++ src/main.js
@@ -14,6 +14,23 @@ class App
     {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
+    
+    setLoading(loading = true)
+    {
+        if (loading === true)
+        {
+            let loadingEl = document.createElement('span')
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        }
+        else
+        {
+            document.getElementById('loading').remove();
+
+        }
+    }
 
     async addRepository(event)
     {
@@ -24,23 +41,32 @@ class App
         {
             return;
         }
+        this.setLoading();
+        try
+        {
+            const response = await api.get(`/repos/${repoInput}`);
+            console.log(response);
+            const {name , description,  html_url, owner:{avatar_url}} = response.data;
 
-        const response = await api.get(`/repos/${repoInput}`);
-        console.log(response);
-        const {name , description,  html_url, owner:{avatar_url}} = response.data;
+            
+            this.repositories.push
+            (
+                {
+                    name,
+                    description,
+                    avatar_url,
+                    html_url,
+                }
+            );
+            this.inputEl.value = '';
+            this.render();
+        }
+        catch(err)
+        {
+            alert('O repositório não existe!');
+        }
+        this.setLoading(false);
 
-        
-        this.repositories.push
-        (
-            {
-                name,
-                description,
-                avatar_url,
-                html_url,
-            }
-        );
-        this.inputEl.value = '';
-        this.render();
     }
 
     render()
@@ -71,6 +97,8 @@ class App
             this.listEl.appendChild(listItemEl);
         });
     }
+    
+
 }
 
 
