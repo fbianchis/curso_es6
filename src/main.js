cffa0f7ac27d1f97e20710de8d22b6f84e3c361d
Felipe Bianchi <fbs96@live.com>
2020-02-13 11:57:31 -0300

    Arquivos até o término da aula Render em tela

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ class App
     {
         this.repositories = [];
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
         this.registerHandlers();
     }
 
@@ -24,8 +25,38 @@ class App
                 html_url: 'http://github.com/rocketseat/rocketseat.com.br'
             }
         );
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render()
+    {
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo =>
+        {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descEl = document.createElement('p');
+            descEl.appendChild(document.createTextNode(repo.description));
+            
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target','__black');
+            linkEl.setAttribute('href',repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
+
 
 new App();
